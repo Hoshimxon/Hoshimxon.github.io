9f83eb9188bade5b5c2a3be7506bccf584b856f0
Hoshimxon <ashuraliyev.1994@mail.ru>
2022-05-23 11:41:04 +0500

    form validation

--- js/index.js
+++ js/index.js
@@ -1,14 +1,33 @@
+const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 function sendEmail() {
-    emailjs.sendForm('service_9pfyr0s', 'template_7gsubki', "#myForm", 'm7Jzqu_aPRNVIN6i7')
-        .then((result) => {
-            const inputs = document.querySelectorAll('#name, #email, #message');
+    let name = document.forms["form"]["name"].value;
+    let email = document.forms["form"]["email"].value;
+    let message = document.forms["form"]["message"].value;
 
-            inputs.forEach(input => {
-                input.value = '';
+    if (name === '') {
+        alert("Name must be filled out!");
+    }else if(email === '') {
+        alert("Email must be filled out!");
+    }else if(message === '') {
+        alert("Message must be filled out!");
+    }else if(!pattern.test(email)) {
+        alert("Please enter valid email!");
+    }else {
+        let send = document.getElementById('send');
+        send.disabled = true;
+        emailjs.sendForm('service_9pfyr0s', 'template_7gsubki', "#myForm", 'm7Jzqu_aPRNVIN6i7')
+            .then((result) => {
+                const inputs = document.querySelectorAll('#name, #email, #message');
+
+                inputs.forEach(input => {
+                    input.value = '';
+                });
+
+                alert('Your message was sent successfully!');
+            }, (error) => {
+                console.log('FAILED...', error.text);
             });
-
-            alert('Your message was sent successfully!');
-        }, (error) => {
-            console.log('FAILED...', error.text);
-        });
+        send.disabled = false;
+    }
 }
